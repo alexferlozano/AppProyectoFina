658617500a06bcee37f86b4c57a648e621c4714f
ErianaEsquivel <19170089@uttcampus.edu.mx>
2020-12-06 15:25:32 -0600

    Pantalla de inicio que dura 4 segundos

---
+++ app/src/main/java/com/example/appproyectofinal/InicioActivity.java
@@ -0,0 +1,32 @@
+package com.example.appproyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class InicioActivity extends AppCompatActivity {
+    private CountDownTimer countDownTimer;
+    private Intent main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        main = new Intent(this, MainActivity.class);
+
+        countDownTimer = new CountDownTimer(4000, 4000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(main);
+            }
+        };
+        countDownTimer.start();
+    }
+}
