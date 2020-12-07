b742e87174938088a44b4e1bcad8117d0faed1ab
Alex Lozano <19170025@uttcampus.edu.mx>
2020-12-07 09:37:42 -0600

    commit de prueba

--- app/src/main/java/com/example/appproyectofinal/UserActivity.java
+++ app/src/main/java/com/example/appproyectofinal/UserActivity.java
@@ -1,15 +1,73 @@
 package com.example.appproyectofinal;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserActivity extends AppCompatActivity {
+    RequestQueue queue;
     String token;
+    TextView name,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        name=(TextView)findViewById(R.id.v_name);
+        email=(TextView)findViewById(R.id.v_correo);
         token=getIntent().getExtras().getString("token");
+    }
+    private void ObtenerDatos()
+    {
+        String url="http://192.168.0.15:8000/api/user/profile";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                name.setText(response.getString("name"));
+                email.setText(response.getString("email"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(UserActivity.this,"No se ha podido realizar la peticion",Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                // Basic Authentication
+                //String auth = "Basic " + Base64.encodeToString(CONSUMER_KEY_AND_SECRET.getBytes(), Base64.NO_WRAP);
+
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        queue.add(request);
     }
 }
