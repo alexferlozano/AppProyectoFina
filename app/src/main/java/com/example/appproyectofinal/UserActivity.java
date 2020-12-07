21be8bdcc38259a7572d28bbb336a8692f5d9e38
ErianaEsquivel <19170089@uttcampus.edu.mx>
2020-12-07 04:51:42 -0600

    Ya se puede ver los datos en el perfil desde el token

--- app/src/main/java/com/example/appproyectofinal/UserActivity.java
+++ app/src/main/java/com/example/appproyectofinal/UserActivity.java
@@ -1,15 +1,69 @@
 package com.example.appproyectofinal;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Header;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserActivity extends AppCompatActivity {
     String token;
+    private TextView name, email;
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         token=getIntent().getExtras().getString("token");
+        name = findViewById(R.id.v_name);
+        email = findViewById(R.id.v_correo);
+        queue = Volley.newRequestQueue(this);
+        Datos();
+    }
+    private void Datos(){
+        String url = "http://192.168.0.103:8000/api/user/profile";
+        //Header Token = new Header(token);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    name.setText(response.getString("name"));
+                    email.setText(response.getString("email"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map getHeaders() throws AuthFailureError{
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        queue.add(request);
     }
 }
