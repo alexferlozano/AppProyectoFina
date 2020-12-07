ce2d1e729a8e7bfea9d1faaa2a28321e31415db3
ErianaEsquivel <19170089@uttcampus.edu.mx>
2020-12-07 16:02:10 -0600

    Cambios de color

--- app/src/main/java/com/example/appproyectofinal/MainActivity.java
+++ app/src/main/java/com/example/appproyectofinal/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.appproyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,4 +34,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Controla la pulsación del boton atrás
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Desea cerrar la aplicación?").setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
