0480aa7d5bdce9448fbe0f52ff7b4dd621453706
Alex Lozano <19170025@uttcampus.edu.mx>
2020-12-06 18:25:12 -0600

    Enmendando mis errores

--- app/src/main/java/com/example/appproyectofinal/LoginActivity.java
+++ app/src/main/java/com/example/appproyectofinal/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.appproyectofinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnRegre = (Button) findViewById(R.id.log_regresar);
+        btnRegre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        Button login = (Button) findViewById(R.id.log_iniciar);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,UserActivity.class);    
+                startActivity(intent);
+            }
+        });
     }
 }
