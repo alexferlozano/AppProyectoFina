cf8ec7c144f10603865cd5119a9c646793930ebc
Alex Lozano <19170025@uttcampus.edu.mx>
2020-12-08 10:55:20 -0600

    Ya prende y apaga los led

--- app/src/main/java/com/example/appproyectofinal/MenuActivity.java
+++ app/src/main/java/com/example/appproyectofinal/MenuActivity.java
@@ -2,8 +2,10 @@ package com.example.appproyectofinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,11 +29,15 @@ public class MenuActivity extends AppCompatActivity {
     String token;
     RequestQueue queue;
     TextView text_grados,t_humedad,t_luz,t_distancia,t_presencia;
+    Switch led1,led2;
+    private CountDownTimer countDownTimer;
     public static final String URL_TEMP = "http://192.168.0.15:8000/api/sensor/temperatura";
     public static final String URL_HUM = "http://192.168.0.15:8000/api/sensor/humedad";
     public static final String URL_LUZ = "http://192.168.0.15:8000/api/sensor/luz";
     public static final String URL_DIS = "http://192.168.0.15:8000/api/sensor/distancia";
     public static final String URL_PRE = "http://192.168.0.15:8000/api/sensor/presencia";
+    public static final String URL_LED1 = "http://192.168.0.15:8000/api/led/1";
+    public static final String URL_LED2 = "http://192.168.0.15:8000/api/led/2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         queue= Volley.newRequestQueue(this);
@@ -42,6 +48,8 @@ public class MenuActivity extends AppCompatActivity {
         t_humedad=(TextView) findViewById(R.id.humedad);
         t_luz=(TextView) findViewById(R.id.luz);
         t_distancia=(TextView) findViewById(R.id.distancia);
+        led1=(Switch) findViewById(R.id.switchLed1);
+        led2=(Switch) findViewById(R.id.switchLed2);
         MostrarDatos(URL_TEMP,text_grados);
         MostrarDatos(URL_HUM,t_humedad);
         MostrarDatos(URL_LUZ,t_luz);
@@ -57,6 +65,60 @@ public class MenuActivity extends AppCompatActivity {
         CerrarSesion();
     }
 
+    public void encenderLed(View view) {
+        if(view.getId()==R.id.switchLed1)
+        {
+            if(led1.isChecked())
+            {
+                ledOnOFF(URL_LED1,"ON");
+            }
+            else
+            {
+                ledOnOFF(URL_LED1,"OFF");
+            }
+        }
+        else
+        {
+            if(led2.isChecked())
+            {
+                ledOnOFF(URL_LED2,"ON");
+            }
+            else
+            {
+                ledOnOFF(URL_LED2,"OFF");
+            }
+        }
+    }
+    private void ledOnOFF(String url, String status)
+    {
+        JSONObject LED=new JSONObject();
+        try {
+            LED.put("value",status);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request= new JsonObjectRequest(Request.Method.POST, url, LED, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(MenuActivity.this,response.toString(),Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MenuActivity.this,error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map getHeaders() throws AuthFailureError{
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        queue.add(request);
+    }
     private void MostrarDatos(String url, TextView datos)
     {
         JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
